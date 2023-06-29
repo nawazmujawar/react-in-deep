e236257ba01ecb1d934c4c97864ffa3f7078d5bf
Nawaz Mujawar <nawazmujawar@hotmail.com>
2023-06-29 13:18:24 +0530

    remove cdn link and add proper imports

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello React In Dept by Namaste React"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
